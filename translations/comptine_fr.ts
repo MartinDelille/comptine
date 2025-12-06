159506bfc952a9cfcdabd098ea9d00c1e48939c7
Martin Delille <martin@delille.org>
2025-12-06 11:15:06 +0100

    Update translations and add CI check for completeness

--- translations/comptine_fr.ts
+++ translations/comptine_fr.ts
@@ -2,80 +2,168 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR">
 <context>
+    <name>BalanceHeader</name>
+    <message>
+        <location filename="../BalanceHeader.qml" line="23"/>
+        <source>Balance:</source>
+        <translation>Solde :</translation>
+    </message>
+    <message>
+        <location filename="../BalanceHeader.qml" line="40"/>
+        <source>%1 operations</source>
+        <translation>%1 opérations</translation>
+    </message>
+</context>
+<context>
+    <name>BudgetView</name>
+    <message>
+        <location filename="../BudgetView.qml" line="93"/>
+        <source>EXCEEDED</source>
+        <translation>DÉPASSÉ</translation>
+    </message>
+    <message>
+        <location filename="../BudgetView.qml" line="130"/>
+        <source>Remaining: %1</source>
+        <translation>Reste : %1</translation>
+    </message>
+    <message>
+        <location filename="../BudgetView.qml" line="130"/>
+        <source>Exceeded: %1</source>
+        <translation>Dépassement : %1</translation>
+    </message>
+    <message>
+        <location filename="../BudgetView.qml" line="130"/>
+        <source>No budget defined</source>
+        <translation>Pas de budget défini</translation>
+    </message>
+    <message>
+        <location filename="../BudgetView.qml" line="143"/>
+        <source>No categories defined</source>
+        <translation>Aucune catégorie définie</translation>
+    </message>
+</context>
+<context>
+    <name>ImportDialog</name>
+    <message>
+        <location filename="../ImportDialog.qml" line="7"/>
+        <source>Import CSV</source>
+        <translation>Importer CSV</translation>
+    </message>
+    <message>
+        <location filename="../ImportDialog.qml" line="54"/>
+        <source>Import into account:</source>
+        <translation>Importer dans le compte :</translation>
+    </message>
+    <message>
+        <location filename="../ImportDialog.qml" line="24"/>
+        <source>New account</source>
+        <translation>Nouveau compte</translation>
+    </message>
+</context>
+<context>
     <name>Main</name>
     <message>
+        <location filename="../Main.qml" line="48"/>
         <source>&amp;File</source>
         <translation>Fichier</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="50"/>
         <source>&amp;New...</source>
         <translation>Nouveau...</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="60"/>
         <source>&amp;Open...</source>
         <translation>Ouvrir...</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="69"/>
         <source>&amp;Save</source>
         <translation>Enregistrer</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="80"/>
         <source>&amp;Save As...</source>
         <translation>Enregistrer sous...</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="86"/>
         <source>&amp;Import CSV...</source>
         <translation>Importer CSV...</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="98"/>
         <source>&amp;Quit</source>
         <translation>Quitter</translation>
     </message>
     <message>
         <source>&amp;Help</source>
-        <translation>Aide</translation>
+        <translation type="vanished">Aide</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="132"/>
         <source>&amp;About</source>
         <translation>À propos</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="130"/>
+        <location filename="../Main.qml" line="137"/>
         <source>&amp;View</source>
         <translation>Affichage</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="110"/>
+        <source>&amp;Undo</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="../Main.qml" line="116"/>
+        <source>&amp;Redo</source>
+        <translation>Rétablir</translation>
+    </message>
+    <message>
+        <location filename="../Main.qml" line="139"/>
         <source>&amp;Operations</source>
         <translation>Opérations</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="144"/>
         <source>&amp;Budget</source>
         <translation>Budget</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="150"/>
         <source>&amp;Previous Month</source>
         <translation>Mois précédent</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="163"/>
         <source>&amp;Next Month</source>
         <translation>Mois suivant</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="180"/>
         <source>Open Budget File</source>
         <translation>Ouvrir un fichier budget</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="190"/>
         <source>Save Budget File</source>
         <translation>Enregistrer le fichier budget</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="210"/>
         <source>Import CSV File</source>
         <translation>Importer un fichier CSV</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="226"/>
         <source>About Comptine</source>
         <translation>À propos de Comptine</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="230"/>
         <source>Comptine v0.1
 
 Personal Budget Management Software
@@ -88,59 +176,123 @@ Logiciel de gestion de budget personnel
 Importez et gérez vos données bancaires.</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="241"/>
+        <source>Unsaved Changes</source>
+        <translation>Modifications non enregistrées</translation>
+    </message>
+    <message>
+        <location filename="../Main.qml" line="242"/>
+        <source>You have unsaved changes. Do you want to save before continuing?</source>
+        <translation>Vous avez des modifications non enregistrées. Voulez-vous enregistrer avant de continuer ?</translation>
+    </message>
+    <message>
+        <location filename="../Main.qml" line="278"/>
         <source>Operations</source>
         <translation>Opérations</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="282"/>
         <source>Budget</source>
         <translation>Budget</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="108"/>
         <source>&amp;Edit</source>
         <translation>Édition</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="123"/>
         <source>&amp;Copy</source>
         <translation>Copier</translation>
     </message>
     <message>
+        <location filename="../Main.qml" line="92"/>
         <source>&amp;Preferences...</source>
         <translation>Préférences...</translation>
     </message>
 </context>
 <context>
-    <name>OperationView</name>
+    <name>MonthSelector</name>
     <message>
-        <source>No account</source>
-        <translation>Aucun compte</translation>
-    </message>
-</context>
-<context>
-    <name>BalanceHeader</name>
-    <message>
-        <source>Balance:</source>
-        <translation>Solde :</translation>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>January</source>
+        <translation>Janvier</translation>
     </message>
     <message>
-        <source>%1 operations</source>
-        <translation>%1 opérations</translation>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>February</source>
+        <translation>Février</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>March</source>
+        <translation>Mars</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>April</source>
+        <translation>Avril</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>May</source>
+        <translation>Mai</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>June</source>
+        <translation>Juin</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>July</source>
+        <translation>Juillet</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>August</source>
+        <translation>Août</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>September</source>
+        <translation>Septembre</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>October</source>
+        <translation>Octobre</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>November</source>
+        <translation>Novembre</translation>
+    </message>
+    <message>
+        <location filename="../MonthSelector.qml" line="14"/>
+        <source>December</source>
+        <translation>Décembre</translation>
     </message>
 </context>
 <context>
     <name>OperationDetails</name>
     <message>
+        <location filename="../OperationDetails.qml" line="28"/>
         <source>Operation Details</source>
         <translation>Détails de l&apos;opération</translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="28"/>
         <source>Multiple Operations</source>
         <translation>Opérations multiples</translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="47"/>
         <source>Selected:</source>
         <translation>Sélectionnées :</translation>
     </message>
     <message numerus="yes">
+        <location filename="../OperationDetails.qml" line="55"/>
         <source>%n operation(s)</source>
         <translation>
             <numerusform>%n opération</numerusform>
@@ -148,152 +300,122 @@ Importez et gérez vos données bancaires.</translation>
         </translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="61"/>
         <source>Total Amount:</source>
         <translation>Montant total :</translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="169"/>
         <source>Select an operation to view details</source>
         <translation>Sélectionnez une opération pour voir les détails</translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="85"/>
         <source>Date:</source>
         <translation>Date :</translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="132"/>
         <source>Amount:</source>
         <translation>Montant :</translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="116"/>
         <source>Category:</source>
         <translation>Catégorie :</translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="125"/>
         <source>Uncategorized</source>
         <translation>Non catégorisé</translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="100"/>
         <source>Description:</source>
         <translation>Description :</translation>
     </message>
     <message>
+        <location filename="../OperationDetails.qml" line="149"/>
         <source>Balance:</source>
         <translation>Solde :</translation>
     </message>
 </context>
 <context>
-    <name>MonthSelector</name>
+    <name>OperationView</name>
     <message>
-        <source>January</source>
-        <translation>Janvier</translation>
+        <location filename="../OperationView.qml" line="12"/>
+        <source>Rename Account</source>
+        <translation>Renommer le compte</translation>
     </message>
     <message>
-        <source>February</source>
-        <translation>Février</translation>
+        <location filename="../OperationView.qml" line="36"/>
+        <source>Account name:</source>
+        <translation>Nom du compte :</translation>
     </message>
     <message>
-        <source>March</source>
-        <translation>Mars</translation>
+        <location filename="../OperationView.qml" line="42"/>
+        <source>Enter account name</source>
+        <translation>Entrez le nom du compte</translation>
     </message>
     <message>
-        <source>April</source>
-        <translation>Avril</translation>
+        <location filename="../OperationView.qml" line="59"/>
+        <source>No account</source>
+        <translation>Aucun compte</translation>
     </message>
     <message>
-        <source>May</source>
-        <translation>Mai</translation>
-    </message>
-    <message>
-        <source>June</source>
-        <translation>Juin</translation>
-    </message>
-    <message>
-        <source>July</source>
-        <translation>Juillet</translation>
-    </message>
-    <message>
-        <source>August</source>
-        <translation>Août</translation>
-    </message>
-    <message>
-        <source>September</source>
-        <translation>Septembre</translation>
-    </message>
-    <message>
-        <source>October</source>
-        <translation>Octobre</translation>
-    </message>
-    <message>
-        <source>November</source>
-        <translation>Novembre</translation>
-    </message>
-    <message>
-        <source>December</source>
-        <translation>Décembre</translation>
-    </message>
-</context>
-<context>
-    <name>BudgetView</name>
-    <message>
-        <source>EXCEEDED</source>
-        <translation>DÉPASSÉ</translation>
-    </message>
-    <message>
-        <source>Remaining: %1</source>
-        <translation>Reste : %1</translation>
-    </message>
-    <message>
-        <source>Exceeded: %1</source>
-        <translation>Dépassement : %1</translation>
-    </message>
-    <message>
-        <source>No budget defined</source>
-        <translation>Pas de budget défini</translation>
-    </message>
-    <message>
-        <source>No categories defined</source>
-        <translation>Aucune catégorie définie</translation>
+        <location filename="../OperationView.qml" line="73"/>
+        <source>Rename</source>
+        <translation>Renommer</translation>
     </message>
 </context>
 <context>
     <name>PreferencesDialog</name>
     <message>
+        <location filename="../PreferencesDialog.qml" line="7"/>
         <source>Preferences</source>
         <translation>Préférences</translation>
     </message>
     <message>
+        <location filename="../PreferencesDialog.qml" line="58"/>
         <source>Language:</source>
         <translation>Langue :</translation>
     </message>
     <message>
+        <location filename="../PreferencesDialog.qml" line="64"/>
+        <location filename="../PreferencesDialog.qml" line="84"/>
         <source>System Default</source>
         <translation>Système par défaut</translation>
     </message>
     <message>
+        <location filename="../PreferencesDialog.qml" line="78"/>
         <source>Theme:</source>
         <translation>Thème :</translation>
     </message>
     <message>
+        <location filename="../PreferencesDialog.qml" line="84"/>
         <source>Light</source>
         <translation>Clair</translation>
     </message>
     <message>
+        <location filename="../PreferencesDialog.qml" line="84"/>
         <source>Dark</source>
         <translation>Sombre</translation>
     </message>
 </context>
 <context>
-    <name>ImportDialog</name>
+    <name>QObject</name>
     <message>
-        <source>Import CSV</source>
-        <translation>Importer CSV</translation>
+        <location filename="../UndoCommands.cpp" line="13"/>
+        <source>Rename account to &quot;%1&quot;</source>
+        <translation>Renommer le compte en « %1 »</translation>
     </message>
-    <message>
-        <source>Import into account:</source>
-        <translation>Importer dans le compte :</translation>
-    </message>
-    <message>
-        <source>New account</source>
-        <translation>Nouveau compte</translation>
+    <message numerus="yes">
+        <location filename="../UndoCommands.cpp" line="40"/>
+        <source>Import %n operation(s)</source>
+        <translation>
+            <numerusform>Importer %n opération</numerusform>
+            <numerusform>Importer %n opérations</numerusform>
+        </translation>
     </message>
 </context>
 </TS>
