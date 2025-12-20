5cc6020fe7109c4bc302e8b9b2a1585124fe56c8
Martin Delille <martin@delille.org>
2025-12-20 22:20:19 +0100

    Move appVersion and appCommitHash to AppState

--- PropertyMacros.h
+++ PropertyMacros.h
@@ -1,14 +1,5 @@
 #pragma once
 
-// Macro for read-write Qt properties with automatic getter, setter, signal, and member
-// Usage: PROPERTY_RW(int, selectedOperationIndex, 0)
-// Generates:
-//   - Q_PROPERTY declaration with WRITE (exposed to QML)
-//   - Type name() const getter
-//   - Q_INVOKABLE void set_name(Type value) setter with change detection
-//   - void nameChanged() signal
-//   - Type _name member variable with default value
-
 #define PROPERTY_RW(Type, name, defaultValue)                           \
   Q_PROPERTY(Type name READ name WRITE set_##name NOTIFY name##Changed) \
 public:                                                                 \
@@ -25,12 +16,13 @@ Q_SIGNALS:                                                              \
 private:                                                                \
   Type _##name = defaultValue;
 
-// Macro for read-only Qt properties with declared getter (implemented in .cpp)
-// Usage: PROPERTY_RO(int, accountCount)
-// Generates:
-//   - Q_PROPERTY declaration (read-only)
-//   - Type name() const getter declaration (you implement in .cpp)
-//   - void nameChanged() signal
+#define PROPERTY_CONSTANT(Type, name, defaultValue) \
+  Q_PROPERTY(Type name READ name CONSTANT)          \
+public:                                             \
+  Type name() const { return _##name; }             \
+                                                    \
+private:                                            \
+  Type _##name = defaultValue;
 
 #define PROPERTY_RO(Type, name)                        \
   Q_PROPERTY(Type name READ name NOTIFY name##Changed) \
@@ -40,15 +32,6 @@ Q_SIGNALS:                                             \
   void name##Changed();                                \
                                                        \
 private:
-
-// Macro for read-write Qt properties with custom getter/setter (implemented in .cpp)
-// Usage: PROPERTY_RW_CUSTOM(int, currentAccountIndex, -1)
-// Generates:
-//   - Q_PROPERTY declaration with WRITE
-//   - Type name() const getter declaration (you implement in .cpp)
-//   - Q_INVOKABLE void set_name(Type value) setter declaration (you implement in .cpp)
-//   - void nameChanged() signal
-//   - Type _name member variable with default value
 
 #define PROPERTY_RW_CUSTOM(Type, name, defaultValue)                    \
   Q_PROPERTY(Type name READ name WRITE set_##name NOTIFY name##Changed) \
