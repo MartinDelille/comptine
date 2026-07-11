22ebbaf6011e6bdcaee49112941d9d97928d0e31
Martin Delille <martin@delille.org>
2026-07-11 17:22:36 +0200

    Use conan profiles

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class ComptineConan(ConanFile):
         self.requires("yaml-cpp/0.8.0")
 
     def generate(self):
-        generator = None if self.settings.os == "Windows" else "Ninja"
+        generator = "Ninja"
         tc = CMakeToolchain(self, generator=generator)
         tc.variables["CMAKE_EXPORT_COMPILE_COMMANDS"] = True
 
